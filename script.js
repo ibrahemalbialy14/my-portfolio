f876db1940e2bf538d6253264d86d74dc2224924
ibrahem albialy <148761529+ibrahemalbialy14@users.noreply.github.com>
2024-07-13 13:55:15 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,22 +4,24 @@ const hiringMe = document.querySelector(".hiring-me");
 const mainMenu = document.querySelector(".main-menu");
 const mainLinks = document.querySelector(".main-links");
 const sections = document.querySelectorAll("section");
-
+const text = document.querySelector(".text")
+console.log(text)
 lis.forEach((li) => {
     li.addEventListener("click", () => {
         lis.forEach((item) => {
             item.classList.remove("active");
             mainLinks.classList.remove("active");
+            text.classList.remove("active");
         });
         li.classList.add("active");
     });
-
 });
 
 contact.addEventListener("click", () => {
     lis.forEach((li) => {
         li.classList.remove("active");
     });
+    text.classList.add("active");
 })
 
 hiringMe.addEventListener("click", () => {
