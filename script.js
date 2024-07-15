35fcb70a2010c99436e195ef556755c8fcdc56ae
ibrahem albialy <148761529+ibrahemalbialy14@users.noreply.github.com>
2024-07-15 19:52:55 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,6 @@ const mainMenu = document.querySelector(".main-menu");
 const mainLinks = document.querySelector(".main-links");
 const sections = document.querySelectorAll("section");
 const text = document.querySelector(".text")
-// const userName = document.getElementById("name").value;
-// var userEmail = document.getElementById("email").value;
-// var userPhone = document.getElementById("phone").value;
-// var userMessage = document.getElementById("message").value;
 lis.forEach((li) => {
     li.addEventListener("click", () => {
         lis.forEach((item) => {
@@ -48,31 +44,12 @@ function changeLinkState () {
     changeLinkState();
 
 window.addEventListener("scroll", changeLinkState);
-// function sendEmail() {
-//     var templateParams = {
-//         to_name: 'Ibrahem Albialy',
-//         from_name: `${userName}`,
-//         email: `${userEmail}`,
-//         phone: `${userPhone}`,
-//         message: `You got a new message: ${userMessage}`
-//     };
-// emailjs.init("DI8AocHz_yjskdOJN");
-// emailjs.send('service_5wusw2t', 'template_ve0ypv9', templateParams)
-//     .then(function(response) {
-//         console.log('SUCCESS!', response.status, response.text);
-//     }, function(error) {
-//         console.log('FAILED...', error);
-//     });
-// }
-
 
 document.getElementById("sendButton").onclick = function() {
-    // الحصول على القيم من الحقول المدخلة
     var userName = document.getElementById("userName").value;
     var userEmail = document.getElementById("userEmail").value;
     var userPhone = document.getElementById("userPhone").value;
     var userMessage = document.getElementById("userMessage").value;
-    // التأكد من أن الحقول ليست فارغة
     if (!userName || !userEmail || !userPhone || !userMessage) {
     console.log('Error: All fields are required.');
     return;
@@ -84,7 +61,6 @@ document.getElementById("sendButton").onclick = function() {
     phone: userPhone,
     message: `You got a new message: ${userMessage}`
     };
-    // تهيئة واستخدام EmailJS
     emailjs.init("DI8AocHz_yjskdOJN");
     emailjs.send('service_5wusw2t', 'template_ve0ypv9', templateParams)
     .then(function(response) {
